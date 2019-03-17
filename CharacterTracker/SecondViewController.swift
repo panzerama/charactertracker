ace26bddf75360e6e9014cf79e3e3e2d0e8172ab
Jason Panzer <jason.drew.panzer@gmail.com>
2019-03-17 16:15:15 -0700

    Initial Commit

---
+++ CharacterTracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CharacterTracker
+//
+//  Created by Jason Panzer on 3/17/19.
+//  Copyright © 2019 Jason Panzer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
